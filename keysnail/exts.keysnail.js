c8d8f3f769009bd591e22d55d4b8ff47d39c5c69
junkw <don.t.be.trapped.by.dogma@gmail.com>
2012-01-11 13:29:18 +0900

    Added auto-install-plugins ext.

--- keysnail/exts.keysnail.js
+++ keysnail/exts.keysnail.js
@@ -1,5 +1,48 @@
 // -*- mode: js2-mode; coding: utf-8; indent-tabs-mode: nil -*-
 
+// Auto install plugins
+// @see http://d.hatena.ne.jp/sr10/20120109/1326108732
+ext.add('auto-install-plugins', function(ev, arg){
+    var urls = [
+        'http://github.com/mooz/keysnail/raw/master/plugins/bmany.ks.js',
+        'https://raw.github.com/gist/1286792/bookmarktag.ks.js',
+        'http://github.com/mooz/keysnail/raw/master/plugins/builtin-commands-ext.ks.js',
+        'http://github.com/mooz/keysnail/raw/master/plugins/caret-hint.ks.js',
+        'https://raw.github.com/gist/1312071/cookie-manager.ks.js',
+        'https://raw.github.com/gist/1031072/encoding-switcher.js',
+        'https://gist.github.com/raw/905297/find.ks.js',
+        'https://raw.github.com/gist/1011984/firefox-addon-manager.ks.js',
+        'https://raw.github.com/gist/1011926/firefox-plugin-manager.ks.js',
+        'http://github.com/mooz/keysnail/raw/master/plugins/github-plugin.ks.js',
+        'https://gist.github.com/raw/895953/history.ks.js',
+        'https://github.com/mooz/keysnail/raw/master/plugins/hok.ks.js',
+        'https://raw.github.com/gist/992351/hok-ex.ks.js',
+        'http://github.com/myuhe/KeySnail_Plugin/raw/master/K2Emacs.ks.js',
+        'https://raw.github.com/gist/1369730/ldrnail.ks.js',
+        'https://gist.github.com/raw/895703/RILnail.ks.js',
+        'http://github.com/mooz/keysnail/raw/master/plugins/_scrollet.ks.js',
+        'http://github.com/mooz/keysnail/raw/master/plugins/site-local-keymap.ks.js',
+        'http://github.com/myuhe/KeySnail_Plugin/raw/master/Tanything.ks.js',
+        'https://raw.github.com/gist/1286784/user-script-manager-ks.js'
+    ];
+
+    function inst (a) {
+        if (a.length == 0) {
+            display.showPopup("auto-install-plugins", "All installation finished.");
+        } else {
+            var url  = a.shift();
+            var path = userscript.pluginDir + userscript.directoryDelimiter + url.match(/[^/]+$/)[0];
+
+        if (plugins.context[path] === undefined) {
+            userscript.installPluginFromURL(url, function(){inst(a);});
+        } else {
+            inst(a);
+        }
+    }
+}
+        inst(urls);
+       }, 'Install plugins automatically if not installed yet.');
+
 // Describe Key
 // @see https://gist.github.com/420462
 ext.add("describe-key", function (ev, arg) {
