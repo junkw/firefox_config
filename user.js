6ecea19eb1ebb5dd33893a98767a099045d6fce9
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2012-03-17 13:41:16 +0900

    Enabled SPDY.

--- user.js
+++ user.js
@@ -4,6 +4,9 @@ user_pref("browser.cache.memory.capacity", -1);
 // Disabel disk cache
 //user_pref("browser.cache.disk.enable", false);
 
+// Enable SPDY protocol
+user_pref("network.http.spdy.enabled", true);
+
 // Use "I'm Feeling Lucky" in location bar
 user_pref("keyword.URL", "http://www.google.com/search?btnI=I%27m+Feeling+Lucky&ie=UTF-8&oe=UTF-8&q=");
 
