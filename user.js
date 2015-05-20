3f7a3745fde4447f78064e4d021987f3cae76ef3
Jumpei KAWAMI <don.t.be.trapped.by.dogma@gmail.com>
2015-05-20 17:23:25 +0900

    Remove obsolete and overlapping 'user_pref'

--- user.js
+++ user.js
@@ -1,31 +1,12 @@
 // Auto-assignment on memory cache
 user_pref("browser.cache.memory.capacity", -1);
 
-// Disabel disk cache
-//user_pref("browser.cache.disk.enable", false);
-
-// Enable SPDY protocol
-user_pref("network.http.spdy.enabled", true);
-user_pref("network.http.spdy.enabled.v3", true);
-
-// Use "I'm Feeling Lucky" in location bar
-user_pref("keyword.URL", "http://www.google.com/search?btnI=I%27m+Feeling+Lucky&ie=UTF-8&oe=UTF-8&q=");
-
-// about:newtab
-user_pref("browser.newtabpage.enabled", true);
-
-// Enable In-Content Preferences
-user_pref("browser.preferences.inContent", true);
-
 // Tab width
 user_pref("browser.tabs.tabMaxWidth", 200);
 user_pref("browser.tabs.tabMinWidth", 100);
 
 // Don't move and resize window
 user_pref("dom.disable_window_move_resize", true);
-
-// Enable PDF.js
-user_pref("pdfjs.disabled", false);
 
 // Local Variables:
 // mode: js2
